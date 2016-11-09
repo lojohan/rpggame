262f318c884c9afdae7ba3303f5533d6f122483b
Johan Lövgren <lojohan@student.chalmers.se>
2016-11-09 15:37:42 +0100

    Fixed main again.

--- src/rpggame/Main.java
+++ src/rpggame/Main.java
@@ -4,16 +4,16 @@ package rpggame;
 public class Main {
     public static void main(String[] args) {
     	
-    	//DialogueGenerator.loadWordClasses();
-    	//DialogueGenerator.loadTemplate();
-    	//DialogueGenerator.populateMap();
-    	//DialogueGenerator.generateDialogues(10000,3,15);
-    	//DialogueGenerator.printDialoguesToFile();
+    	DialogueGenerator.loadWordClasses();
+    	DialogueGenerator.loadTemplate();
+    	DialogueGenerator.populateMap();
+    	DialogueGenerator.generateDialogues(10000,3,15);
+    	DialogueGenerator.printDialoguesToFile();
     	
-    	//for (int i = 0 ; i < 10; i++) {
-	    //	System.out.println("A random name: " + NameGenerator.generateRandomName());
-	    //	System.out.println("A random place: " + NameGenerator.generateRandomPlaceName());
-    	//}
+    	for (int i = 0 ; i < 10; i++) {
+	    	System.out.println("A random name: " + NameGenerator.generateRandomName());
+	    	System.out.println("A random place: " + NameGenerator.generateRandomPlaceName());
+    	}
     	
     	MapGenerator.generate(16, 20, 10, 2);
     }
